4ee5b3da834c412f463e3128b3a17b517a528444
sykuang <sykuang.tw@gmail.com>
2020-06-13 03:18:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,14 @@ mkdir -p $XDG_CONFIG_HOME/nvim/
 ln -s $SCRIPT_PATH/init.vim $XDG_CONFIG_HOME/nvim/init.vim
 fi
 
-if [[ ! -f $HOME/.gitignore ]];then
+if [[ ! -f $HOME/.gitconfig ]];then
   ln -s $SCRIPT_PATH/gitconfig $HOME/.gitconfig
+fi
+
+if [[ ! -f $HOME/.zshrc ]];then
+  ln -s $SCRIPT_PATH/zshrc/zshrc $HOME/.zshrc
+fi
+
+if [[ ! -f $HOME/.vimrc ]];then
+  ln -s $SCRIPT_PATH/vimrc/vimrc $HOME/.vimrc
 fi
