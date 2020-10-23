245c57166483d69a15ea699dfef1698943c06b0a
sykuang <sykuang.tw@gmail.com>
2020-10-23 14:31:33 +0800

    Add error checkng for install.sh

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+## Installation
+1. Install
+`git clone --recursive https://github.com/sykuang/dotfiles.git $HOME/.dotfiles;./$HOME/.dotfiles/install.sh`
+
--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+if [[ -z `which curl` && -z `which wget` && -z `which lftp` ]];then
+  echo "Please install one of below command: curl,wget or lftp"
+  exit 1
+fi
 if [[ ! -f $HOME/.config/alacritty/alacritty.yml ]];then
 mkdir -p $XDG_CONFIG_HOME/alacritty/
 ln -s $SCRIPT_PATH/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
@@ -20,9 +24,21 @@ if [[ ! -f $HOME/.config/starship.toml ]];then
 fi
 
 if [[ ! -f $HOME/.zshrc ]];then
+  if [ -z `which exa` ];then
+    echo "Please install exa for zshrc"
+    exit 1
+  fi
+  if [ -z `which svn` ];then
+    echo "Please install svn for zshrc"
+    exit 1
+  fi
   ln -s $SCRIPT_PATH/zshrc/zshrc $HOME/.zshrc
 fi
 
 if [[ ! -f $HOME/.vimrc ]];then
   ln -s $SCRIPT_PATH/vimrc/vimrc $HOME/.vimrc
+  pip install pynvim
+  if [[ -f $SCRIPT_PATH/vimrc/install.sh ]];then
+    $SCRIPT_PATH/vimrc/install.sh
+  fi
 fi
