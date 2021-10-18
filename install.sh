293301b6f5d92a6bcef6f29d1a558d77fd6dba61
sykuang <sykuang.tw@gmail.com>
2021-10-18 21:57:58 +0800

    Update install.sh, vim and zhsrc

--- install.sh
+++ install.sh
@@ -2,39 +2,33 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+
 if [[ -z `which curl` && -z `which wget` && -z `which lftp` ]];then
   echo "Please install one of below command: curl,wget or lftp"
   exit 1
 fi
-if [[ ! -f $HOME/.config/alacritty/alacritty.yml ]];then
-mkdir -p $XDG_CONFIG_HOME/alacritty/
-ln -s $SCRIPT_PATH/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
-fi
-if [[ ! -f $XDG_CONFIG_HOME/nvim ]];then
-  ln -s $SCRIPT_PATH/nvim-config $XDG_CONFIG_HOME/nvim
-  if [[ -f $SCRIPT_PATH/nvim-config/install.sh ]];then
-  $SCRIPT_PATH/nvim-config/install.sh
+
+if command -v alacritty &> /dev/null;then
+  if [[ ! -f $HOME/.config/alacritty/alacritty.yml ]];then
+    mkdir -p $XDG_CONFIG_HOME/alacritty/
+    ln -s $SCRIPT_PATH/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
   fi
 fi
 
+# Setup for neovim
+if command -v nvim &> /dev/null;then
+  $SCRIPT_PATH/nvim-config/install.sh
+fi
+
+# Setup for git config
 if [[ ! -f $HOME/.gitconfig ]];then
   ln -s $SCRIPT_PATH/gitconfig $HOME/.gitconfig
 fi
 
+# Setup for starship
 if [[ ! -f $HOME/.config/starship.toml ]];then
   ln -s $SCRIPT_PATH/starship.toml $HOME/.config/starship.toml
 fi
 
-# if [[ ! -f $HOME/.config/nvim/coc-settings.json ]];then
-  # ln -s $SCRIPT_PATH/coc-settings.json $HOME/.config/nvim/coc-settings.json
-# fi
-
+# Setup zshrc
 [[ -f $SCRIPT_PATH/zshrc/install.sh ]] && $SCRIPT_PATH/zshrc/install.sh
-
-if [[ ! -f $HOME/.vimrc ]];then
-  ln -s $SCRIPT_PATH/vimrc/vimrc $HOME/.vimrc
-  pip install pynvim
-  if [[ -f $SCRIPT_PATH/vimrc/install.sh ]];then
-    $SCRIPT_PATH/vimrc/install.sh
-  fi
-fi
