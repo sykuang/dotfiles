ecc56264a0481d5b76e072334ba647f8485cf3cf
sykuang <sykuang.tw@gmail.com>
2022-09-01 01:31:58 +0800

    Refine README and install.sh

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Dotfiles
 ## Installation
 1. Install
-`git clone --recursive https://github.com/sykuang/dotfiles.git $HOME/.dotfiles;$HOME/.dotfiles/install.sh`
+```
+git clone --recurse-submodules https://github.com/sykuang/dotfiles.git $HOME/.dotfiles;$HOME/.dotfiles/install.sh
+```
 
--- install.sh
+++ install.sh
@@ -3,27 +3,22 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
-if [[ -z `which curl` && -z `which wget` && -z `which lftp` ]];then
-  echo "Please install one of below command: curl, wget or lftp"
-  exit 1
-fi
-
 if command -v alacritty &> /dev/null;then
   if [[ ! -f $HOME/.config/alacritty/alacritty.yml ]];then
-    mkdir -p $XDG_CONFIG_HOME/alacritty/
-    ln -s $SCRIPT_PATH/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+    mkdir -p "$XDG_CONFIG_HOME/alacritty/"
+    ln -s "$SCRIPT_PATH/alacritty.yml" "$XDG_CONFIG_HOME/alacritty/alacritty.yml"
   fi
 fi
 
 # Setup for neovim
 if command -v nvim &> /dev/null;then
-  $SCRIPT_PATH/nvim-config/install.sh
+  "$SCRIPT_PATH/nvim-config/install.sh"
 fi
 
 # Setup for git config
 if [[ ! -f $HOME/.gitconfig ]];then
-  ln -s $SCRIPT_PATH/gitconfig $HOME/.gitconfig
+  ln -s "$SCRIPT_PATH/gitconfig" "$HOME/.gitconfig"
 fi
 
 # Setup zshrc
-[[ -f $SCRIPT_PATH/zshrc/install.sh ]] && $SCRIPT_PATH/zshrc/install.sh
+[[ -f "$SCRIPT_PATH/zshrc/install.sh" ]] && "$SCRIPT_PATH/zshrc/install.sh"
